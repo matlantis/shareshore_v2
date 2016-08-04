13dda2b42ae442bcc27fda0598d34fbe1647da9d
matlantis <martinstefanputtke@posteo.de>
2016-08-04 17:58:45 +0200

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     get "users/:id", action: :show, controller: 'users/registrations', as: 'user'
   end
 
+  get "/pages/:page" => "pages#show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
