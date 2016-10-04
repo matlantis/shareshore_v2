4e3a3cdf8247f599652b7d285d8746a6831d9e5e
matlantis <martinstefanputtke@posteo.de>
2016-10-04 16:54:59 +0200

    fixed problem with article model initialization

--- app/models/article.rb
+++ app/models/article.rb
@@ -15,6 +15,14 @@ class Article < ActiveRecord::Base
   validates :rate_eur, numericality: { greater_than_or_equal_to: 0 }
   validates :rate_interval, presence: true, length: { minimum: 1, maximum: 50 }
   validates :quality, numericality: { only_integer: true, minimum: 1, maximum: 5 }
+
+  after_initialize :init
+  
+  def init
+    self.rate_eur ||= 1
+    self.rate_interval ||= "day"
+    self.quality ||= 3
+  end
   
   def self.search(search)
     where("lower(title) LIKE ?", "%#{search.downcase}%") 
