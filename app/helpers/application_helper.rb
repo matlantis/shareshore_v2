05af5a3b1508af92ab872a676e256558845fec33
matlantis <martinstefanputtke@posteo.de>
2016-11-02 13:07:01 +0100

    hilfe panel per helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,20 @@ module ApplicationHelper
   def underscore(txt)
     txt.squish.downcase.tr(" ","_")
   end
+
+  def tipp_panel(title: "Tipp", &block)
+    capture do
+      content_tag :div, class: ["panel","panel-default"] do
+        
+        txt1 = content_tag :div, class: "panel-heading" do
+          content_tag(:h4, title)
+        end
+        txt2 = content_tag :div, class: "panel-body" do
+          capture(&block)
+        end
+
+        txt1.concat(txt2)
+      end          
+    end
+  end
 end
