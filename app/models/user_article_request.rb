0855eedaa19e3b0ae28e49003fda8776aa3da9c0
matlantis <martinstefanputtke@posteo.de>
2016-11-26 18:36:15 +0100

    hide real names

--- app/models/user_article_request.rb
+++ app/models/user_article_request.rb
@@ -1,4 +1,10 @@
 class UserArticleRequest < ActiveRecord::Base
   belongs_to :sender, class_name: "User"
   belongs_to :article
+
+  after_initialize :init
+
+  def init
+    with_name = false # always hide
+  end
 end
