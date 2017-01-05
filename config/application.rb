bd9456d5643539bb4f924522016ee4fe89a385d3
matlantis <martinstefanputtke@posteo.de>
2017-01-05 19:36:15 +0100

    changes due to comparison with rails 5 vanilla projekt

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,6 @@ module ShareshoreV2
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     # images in vendor assets images leaflet
     config.assets.paths << Rails.root.join("vendor", "assets", "images", "leaflet")
   end
