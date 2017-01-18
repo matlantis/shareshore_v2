e722e1aaa8f90d46fcccefb7bcf371dc9d1b64d5
matlantis <martinstefanputtke@posteo.de>
2017-01-18 12:34:31 +0100

    validation for contact method in requests

--- app/assets/javascripts/user_article_requests.js
+++ app/assets/javascripts/user_article_requests.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     // hide the form and show the send message
     parent_div.find('.waiting').hide(200)
     parent_div.find('.request-was-send').show(200)
+    parent_div.find('.error-msg').text("").hide(200)
   });
 
   // ajax response for user article requests
@@ -15,14 +16,16 @@ $(document).ready(function() {
     // hide the form and show the send message
     parent_div.find('.send-request').hide(200)
     parent_div.find('.waiting').show(200)
+    parent_div.find('.error-msg').text("").hide(200)
   });
 
   // ajax response for user article requests
-  $('.new_user_article_request').on('ajax:error', function () {
+  $('.new_user_article_request').on('ajax:error', function (e, xhr, status, error) {
     // find a parent element
     parent_div = $(this).parentsUntil('.request-panel').parent();
     // hide the form and show the send message
     parent_div.find('.waiting').hide(200)
     parent_div.find('.send-request').show(200)
+    parent_div.find('.error-msg').text(xhr.responseText).show(200)
   });
 })
--- app/models/user_article_request.rb
+++ app/models/user_article_request.rb
@@ -1,7 +1,17 @@
+class ContactMethodValidator < ActiveModel::Validator
+  def validate(request)
+    if !request.with_phoneno && !request.with_email
+      request.errors[:base] << I18n.t("user_article_requests.no_contact_method")
+    end
+  end
+end
+
 class UserArticleRequest < ApplicationRecord
   belongs_to :user, inverse_of: :user_article_requests
   belongs_to :article
 
+  validates_with ContactMethodValidator
+  
   after_initialize :init
 
   def init
