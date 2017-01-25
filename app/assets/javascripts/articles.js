49db9db2a50c96d92d3cc60e289dadfde71020d1
matlantis <martinstefanputtke@posteo.de>
2017-01-25 20:21:01 +0100

    besseres layout

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,11 +1,7 @@
 $(document).on('turbolinks:load', function(){
-  console.log("runnnn")
   // hide all item edit forms on load
   $(".article_edit").hide();
   $("#article_new").hide();
-
-  // hide items that have class to_hide
-  $('.to_hide').hide();
 
   // new_from_stockitems hide all article counters where number is 0
   $(".article_count[data=0]").hide();
