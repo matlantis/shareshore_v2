a6a0ac83a85c72734d776fead6210f1abeaa8dfd
matlantis <martinstefanputtke@posteo.de>
2017-01-26 13:43:49 +0100

    global map

--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -5,5 +5,20 @@ $(document).on('turbolinks:load', function() {
     $('#tab-articles').toggleClass('scrollable-results');
     $('#tab-locations').toggleClass('scrollable-results');
   })
-                         
+
+  $('#search_use_location').on('click', function(e) {
+    $('#search_location_id').closest('.form-group').toggle(e.target.checked);
+    $('#search_address').closest('.form-group').toggle(!e.target.checked);
+  });
+
+  $(".toggle-map").on('click', function(e) {
+    $('.main-content').toggleClass('map_visible')
+    $('.all_but_map').toggleClass('map_visible')
+
+    // scroll the view down by map height when map is opening
+    if ( $('.all_but_map').hasClass('map_visible') )
+      $('html, body').animate({
+        scrollTop: '+=' + $(window).height() * 0.4
+      }, 200);
+  })
 })
