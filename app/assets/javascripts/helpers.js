d57832ed120c546a9bbbbaa8dff1780e6ae1b3b8
Kapuze Martin <m.s.p@posteo.de>
2020-04-04 15:28:37 +0200

    disable all hidden elements
    - there were problems with checkboxes that could still be clicked

--- app/assets/javascripts/helpers.js
+++ app/assets/javascripts/helpers.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
+
     // class to toggle the visibility of some target
     $("body").on("click", ".mp-toggle-visibility", function(e) {
         e.preventDefault();
         target = $(this).attr("data-toggle-target");
-        $(target).toggle(000);
+        $(target).toggle();
+        // disable hidden elements (like checkboxes)
+        $(target).each(function() {
+            $(this).attr("disabled", ! $(this).attr("disabled"));
+        });
     });
 
+    // disable all hidden elements
+    $(".to-hide").attr("disabled", "disabled");
 });
--- app/assets/javascripts/registrations.js
+++ app/assets/javascripts/registrations.js
@@ -1,5 +1,7 @@
 var onUserUpdate = function(newtext, lat, lng) {
     $('#user_form').replaceWith(newtext);
+    // disable all hidden elements
+    $(".to-hide").attr("disabled", "disabled");
     drawmap_leaflet("mapid", marker_defs, marker_defs_center, bounds);
     markers[0].setLatLng({lat: lat, lng: lng});
     map.setView(markers[0].getLatLng(), defaultZoom);
